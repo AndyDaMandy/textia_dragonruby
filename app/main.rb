cddeb928c59cfedcd9374e3bed61132e6edd3c6b
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-04-15 19:37:05 -0400

    Setting up a text holder

--- app/main.rb
+++ app/main.rb
@@ -32,7 +32,24 @@ def level_up(char)
   char.exp += 1
 end
 
+def main_screen(args, state)
+  x = 500
+  y = 540
+  if state == 0
+    args.outputs.labels << [x, y, 'Amaryllis Town\n
+      Shaman: "Hello Ando and Marie, today is the day of your pilgrimage."\n
+      Our town needs a new crystal and you both are going to the Crystal Lake to gather one for us
+      Without it we cannot protect ourselves from monsters much longer.
+      To access the menu, click the "Menu" button above. You can check your items, party, skills, etc.
+      "If you click the tutorial button you can learn a lot about how to survive. Dont go into the wilderness unprepared! You can access it at any time, so if youre ever confused you can open it again."
+      "If youd like to move on, click below, or click the shop button above"', 5, 1]
+  elsif state == 1
+    args.outputs.labels << [x, y, "testing this!", 5, 1]
+  end
+end
+
 def tick args
+  #chars states
   args.state.ando ||= {
     name: 'Ando',
     level: 1,
@@ -109,11 +126,42 @@ def tick args
     weapon: 'Text',
     type: 'Player'
   }
-  show_status(args, args.state.ando, 1)
-  show_status(args, args.state.marie, 2)
-  show_status(args, args.state.julie, 3)
+  #enemies states
+  # length of characters on line
+  max_character_length = 80
+  # line height
+  line_height = 25
+
+  #args.state.screen ||= 0
+  #main_screen(args, args.state.screen)
+  #show_status(args, args.state.ando, 1)
+  #show_status(args, args.state.marie, 2)
+  #show_status(args, args.state.julie, 3)
+  # create a really long string
+  args.state.really_long_string =  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate viverra metus et vehicula. Aenean quis accumsan dolor. Nulla tempus, ex et lacinia elementum, nisi felis ullamcorper sapien, sed sagittis sem justo eu lectus. Etiam ut vehicula lorem, nec placerat ligula. Duis varius ultrices magna non sagittis. Aliquam et sem vel risus viverra hendrerit. Maecenas dapibus congue lorem, a blandit mauris feugiat sit amet."
+  args.state.really_long_string += "\n"
+  args.state.really_long_string += "Sed quis metus lacinia mi dapibus fermentum nec id nunc. Donec tincidunt ante a sem bibendum, eget ultricies ex mollis. Quisque venenatis erat quis pretium bibendum. Pellentesque vel laoreet nibh. Cras gravida nisi nec elit pulvinar, in feugiat leo blandit. Quisque sodales quam sed congue consequat. Vivamus placerat risus vitae ex feugiat viverra. In lectus arcu, pellentesque vel ipsum ac, dictum finibus enim. Quisque consequat leo in urna dignissim, eu tristique ipsum accumsan. In eros sem, iaculis ac rhoncus eu, laoreet vitae ipsum. In sodales, ante eu tempus vehicula, mi nulla luctus turpis, eu egestas leo sapien et mi."
+
+  
+
+  long_string = args.state.really_long_string
+
+  # API: args.string.wrapped_lines string, max_character_length
+  long_strings_split = args.string.wrapped_lines long_string, max_character_length
+
+  # render a label for each line and offset by the line_height
+  args.outputs.labels << long_strings_split.map_with_index do |s, i|
+    {
+      x: 60,
+      y: 60.from_top - (i * line_height),
+      text: s
+    }
+  end
   if args.inputs.keyboard.key_down.z
     level_up(args.state.ando)
+  end
+  if args.inputs.keyboard.key_down.j
+    args.state.screen += 1
   end
  # args.outputs.labels  << [640, 540, args.state.ando.name, 5, 1]
  # args.outputs.labels  << [640, 500, "hp: #{args.state.ando.hp}", 5, 1]
